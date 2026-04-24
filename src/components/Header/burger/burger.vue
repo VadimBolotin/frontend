fd7eed82b07b99e40652535deaf45e2be16b4159
Vadim <Bolotin.Vadim.7@yandex.ru>
2026-04-25 00:13:46 +0300

    feat: добавил на мобилке выезжающее меню

--- src/components/Header/burger/burger.vue
+++ src/components/Header/burger/burger.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import { Burger } from '@/assets';
     import styles from './styles.module.scss'
-    import { ref } from 'vue';
+    import { onMounted, onUnmounted, ref } from 'vue';
 
     interface MenuItem {
         id: number, 
@@ -15,17 +15,47 @@
     ]
 
     const activeId = ref(1)
+    const isMenuOpen = ref(false)
+    const isMobile = ref(false)
+
+
+    const checkMobile = () => {
+        isMobile.value = window.innerWidth <= 1024 
+    }
 
     const handleItemClick = (item: MenuItem) => {
         console.log('Clicked:', item.title)
         activeId.value = item.id
+        isMenuOpen.value = false
     }
+
+    const toggleMenu = () => {
+        if (isMobile.value) {
+            isMenuOpen.value = !isMenuOpen.value
+        }
+    }
+
+    const closeMenu = () => {
+        isMenuOpen.value = false
+    }
+
+    onMounted(() => {
+        checkMobile()
+        window.addEventListener('resize', checkMobile)
+    })
+
+    onUnmounted(() => {
+        window.removeEventListener('resize', checkMobile)
+    })
 
 </script>
 
 <template>
     <div :class="styles.burger">
-        <Burger/>
+        <div @click="toggleMenu">
+            <Burger/>
+        </div>
+        
 
         <ul :class="styles.items">
             <li
@@ -40,5 +70,27 @@
                 {{ item.title }}
             </li>
         </ul>
+
+        <Transition name="mobile-menu">
+            <div v-if="isMenuOpen" :class="styles.mobileMenu">
+                <div :class="styles.closeButton" @click="closeMenu">
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul :class="styles.mobileItems">
+                    <li
+                        v-for="item in burgerItems" 
+                        :key="item.id"
+                        :class="[
+                            styles.mobileItem,
+                            { [styles.active]: activeId === item.id }
+                        ]"
+                        @click="handleItemClick(item)"
+                    >
+                        {{ item.title }}
+                    </li>
+                </ul>
+            </div>
+        </Transition>
     </div>
 </template>
